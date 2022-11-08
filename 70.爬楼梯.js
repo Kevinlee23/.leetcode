043f6b1f1bdb53b907f60547566857cdfee3444a
lixiuxian@24556 <lixiuxian@talkweb.com.cn>
2022-11-08 09:26:09 +0800

    update: q70 ac

--- 70.爬楼梯.js
+++ 70.爬楼梯.js
@@ -10,7 +10,15 @@
  * @return {number}
  */
 var climbStairs = function(n) {
+  if (n === 0 || n === 1) return 1;
+  var dp = new Array(n);
+  dp[0] = 1;
+  dp[1] = 1;
+  for (var i = 2; i <= n; i++) {
+    dp[i] = dp[i-1] + dp[i-2];
+  }
 
+  return dp[n];
 };
 // @lc code=end
 
